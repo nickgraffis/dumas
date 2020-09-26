6b80e6d09828144f2452f82c4c519ec493e09352
Nick Graffis <nicholasgraffis@gmail.com>
2020-09-26 11:22:37 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,203 @@
+var wikiText = `The Battle of Stamford Bridge (Old English: Gefeoht æt Stanfordbrycge) took place at the village of Stamford Bridge, East Riding of Yorkshire, in England on 25 September 1066, between an English army under King Harold Godwinson and an invading Norwegian force led by King Harald Hardrada and the English king's brother Tostig Godwinson. After a bloody battle, both Hardrada and Tostig along with most of the Norwegians were killed. Although Harold Godwinson repelled the Norwegian invaders, his army was defeated by the Normans at Hastings less than three weeks later. The battle has traditionally been presented as symbolising the end of the Viking Age, although major Scandinavian campaigns in Britain and Ireland occurred in the following decades, such as those of King Sweyn Estrithson of Denmark in 1069–1070 and King Magnus Barefoot of Norway in 1098 and 1102–1103.
+The death of King Edward the Confessor of England in January 1066 had triggered a succession struggle in which a variety of contenders from across north-western Europe fought for the English throne. These claimants included the King of Norway, Harald Hardrada. According to the Anglo-Saxon Chronicle Manuscript D (p. 197),[3] the Norwegians assembled a fleet of 300 ships to invade England. The authors, however, did not seem to differentiate between warships and supply ships. In King Harald's Saga, Snorri Sturluson states, "... it is said that King Harald had over two hundred ships, apart from supply ships and smaller craft."[4] Combined with reinforcements picked up in Orkney, the Norwegian army most likely numbered between 7,000 and 9,000 men. Arriving off the English coast in September Hardrada was joined by further forces recruited in Flanders and Scotland by Tostig Godwinson.[5] Tostig was at odds with his elder brother Harold (who had been elected king by the Witenagemot on the death of Edward). Having been ousted from his position as Earl of Northumbria and exiled in 1065, Tostig had mounted a series of abortive attacks on England in the spring of 1066.[6]
+
+In the late summer of 1066, the invaders sailed up the Ouse before advancing on York. On 20 September they defeated a northern English army led by Edwin, Earl of Mercia, and his brother Morcar, Earl of Northumbria, at the Battle of Fulford, outside York. Following this victory they received the surrender of York. Having briefly occupied the city and taken hostages and supplies from the city they returned towards their ships at Riccall. They offered peace to the Northumbrians in exchange for their support for Hardrada's bid for the throne, and demanded further hostages from the whole of Yorkshire.[7]
+
+The sudden appearance of the English army caught the Norwegians by surprise.[14] The English advance was then delayed by the need to pass through the choke-point presented by the bridge itself. The Anglo-Saxon Chronicle has it that a giant Norse axeman (possibly armed with a Dane Axe) blocked the narrow crossing and single-handedly held up the entire English army. The story is that this axeman cut down up to 40 Englishmen and was defeated only when an English soldier floated under the bridge in a half-barrel and thrust his spear through the planks in the bridge, mortally wounding the axeman.[15]
+
+This delay had allowed the bulk of the Norse army to form a shieldwall to face the English attack. Harold's army poured across the bridge, forming a line just short of the Norse army, locked shields and charged. The battle went far beyond the bridge itself, and although it raged for hours, the Norse army's decision to leave their armour behind left them at a distinct disadvantage. Eventually, the Norse army began to fragment and fracture, allowing the English troops to force their way in and break up the Scandinavians' shield wall. Completely outflanked, and with Hardrada killed with an arrow to his windpipe and Tostig slain, the Norwegian army disintegrated and was virtually annihilated.[16]
+
+In the later stages of the battle, the Norwegians were reinforced by troops who had been guarding the ships at Riccall, led by Eystein Orre, Hardrada's prospective son-in-law. Some of his men were said to have collapsed and died of exhaustion upon reaching the battlefield. The remainder were fully armed for battle. Their counter-attack, described in the Norwegian tradition as "Orre's Storm", briefly checked the English advance, but was soon overwhelmed and Orre was slain. The Norwegian army were routed. As given in the Chronicles, pursued by the English army, some of the fleeing Norsemen drowned whilst crossing rivers.[1]
+
+So many died in an area so small that the field was said to have been still whitened with bleached bones 50 years after the battle.[17][18]
+According to Snorri Sturluson, before the battle a single man rode up alone to Harald Hardrada and Tostig. He gave no name, but spoke to Tostig, offering the return of his earldom if he would turn against Hardrada. Tostig asked what his brother Harold would be willing to give Hardrada for his trouble. The rider replied "Seven feet of English ground, as he is taller than other men." Then he rode back to the Saxon host. Hardrada was impressed by the rider's boldness, and asked Tostig who he was. Tostig replied that the rider was Harold Godwinson himself.[13] According to Henry of Huntingdon, Harold said "Six feet of ground or as much more as he needs, as he is taller than most men."[citation needed]
+
+The sudden appearance of the English army caught the Norwegians by surprise.[14] The English advance was then delayed by the need to pass through the choke-point presented by the bridge itself. The Anglo-Saxon Chronicle has it that a giant Norse axeman (possibly armed with a Dane Axe) blocked the narrow crossing and single-handedly held up the entire English army. The story is that this axeman cut down up to 40 Englishmen and was defeated only when an English soldier floated under the bridge in a half-barrel and thrust his spear through the planks in the bridge, mortally wounding the axeman.[15]
+
+This delay had allowed the bulk of the Norse army to form a shieldwall to face the English attack. Harold's army poured across the bridge, forming a line just short of the Norse army, locked shields and charged. The battle went far beyond the bridge itself, and although it raged for hours, the Norse army's decision to leave their armour behind left them at a distinct disadvantage. Eventually, the Norse army began to fragment and fracture, allowing the English troops to force their way in and break up the Scandinavians' shield wall. Completely outflanked, and with Hardrada killed with an arrow to his windpipe and Tostig slain, the Norwegian army disintegrated and was virtually annihilated.[16]
+
+In the later stages of the battle, the Norwegians were reinforced by troops who had been guarding the ships at Riccall, led by Eystein Orre, Hardrada's prospective son-in-law. Some of his men were said to have collapsed and died of exhaustion upon reaching the battlefield. The remainder were fully armed for battle. Their counter-attack, described in the Norwegian tradition as "Orre's Storm", briefly checked the English advance, but was soon overwhelmed and Orre was slain. The Norwegian army were routed. As given in the Chronicles, pursued by the English army, some of the fleeing Norsemen drowned whilst crossing rivers.[1]
+
+So many died in an area so small that the field was said to have been still whitened with bleached bones 50 years after the battle.[17][18]
+King Harold accepted a truce with the surviving Norwegians, including Harald's son Olaf and Paul Thorfinnsson, Earl of Orkney. They were allowed to leave after giving pledges not to attack England again. The losses the Norwegians had suffered were so severe that only 24 ships from the fleet of over 300 were needed to carry the survivors away.[1] They withdrew to Orkney, where they spent the winter, and in the spring Olaf returned to Norway. The kingdom was then divided and shared between him and his brother Magnus, whom Harald had left behind to govern in his absence.[19]
+
+Harold's victory was short-lived. Three days after the battle, on 28 September, a second invasion army led by William, Duke of Normandy, landed in Pevensey Bay, Sussex, on the south coast of England. Harold had to immediately turn his troops around and force-march them southwards to intercept the Norman army.[20] Less than three weeks after Stamford Bridge, on 14 October, the English army was decisively defeated and King Harold II fell in action at the Battle of Hastings,[21] beginning the Norman conquest of England, a process facilitated by the heavy losses amongst the English military commanders.[`;
+
+var text = 'This is a short Snippet of text!';
+
+var stopwords = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', "you're", "you've", "you'll", "you'd", 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', "she's", 'her', 'hers', 'herself', 'it', "it's", 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', "that'll", 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', "don't", 'should', "should've", 'now', 'd', 'll', 'm', 'o', 're', 've', 'y', 'ain', 'aren', "aren't", 'couldn', "couldn't", 'didn', "didn't", 'doesn', "doesn't", 'hadn', "hadn't", 'hasn', "hasn't", 'haven', "haven't", 'isn', "isn't", 'ma', 'mightn', "mightn't", 'mustn', "mustn't", 'needn', "needn't", 'shan', "shan't", 'shouldn', "shouldn't", 'wasn', "wasn't", 'weren', "weren't", 'won', "won't", 'wouldn', "wouldn't"];
+
+function dot(vector1, vector2) {
+  var result = 0;
+  for (var i = 0; i < vector1.length; i++) {
+    result += vector1[i] * vector2[i];
+  }
+  return result;
+}
+
+function sentenceTokinization (text) {
+  return text.replace(/([.?!])\s*(?=[A-Z])/g, "$1|").split("|");
+};
+
+var wikiSentences = sentenceTokinization(wikiText);
+
+function tokenizeWords(text) {
+  var processedText = text.replace(/[^a-zA-Z ]/g, "");
+  processedText = processedText.replace(/\d+/g, '');
+  var words = processedText.split(/\W+/).filter(function(token) {
+      token = token.toLowerCase();
+      return token.length >= 2 && stopwords.indexOf(token) == -1;
+  });
+  return words;
+}
+var nGramNumber = 2
+
+function generate_ngrams (words, n) {
+  var nGramsList = [];
+  for (let i = 0; i < words.length; i++) {
+    var nGramWord = words.slice(i, i + nGramNumber).join(' ');
+    if (nGramWord.split(' ').length >= 2) {
+      nGramsList.push(nGramWord);
+    }
+  }
+  return nGramsList;
+}
+
+function frequencyOfWords(words) {
+  var freq = {};
+  var uniqueWords = [];
+  for (let i = 0; i < words.length; i++) {
+    if (!uniqueWords.includes(words[i])) {
+      uniqueWords.push(words[i]);
+    }
+  }
+  for (let i = 0; i < words.length; i++) {
+    var result = words.filter(word => word === words[i]);
+    freq[words[i]] = result.length;
+  }
+  return freq;
+}
+
+function tfScore (word, sentence) {
+  var wordFreqInSentence = 1;
+  var sentenceInNGrams = generate_ngrams(sentence.split(' '), nGramNumber);
+  for (let i = 0; i < sentenceInNGrams.length; i++) {
+    if (word === sentenceInNGrams[i]) {
+      wordFreqInSentence++;
+    }
+  }
+  var tf = wordFreqInSentence / sentenceInNGrams.length;
+  return tf;
+}
+
+function idfScore (word, sentences) {
+  var noOfSentencesContainingWord = 1;
+  for (let i = 0; i < sentences.length; i++) {
+    let eachSentenceInNGrams = generate_ngrams(sentences[i].split(' '), nGramNumber);
+    for (let k = 0; k < eachSentenceInNGrams.length; k++) {
+      if (word === eachSentenceInNGrams[k]) {
+        noOfSentencesContainingWord++;
+      }
+    }
+  }
+  var idf = Math.log10(sentences.length / noOfSentencesContainingWord);
+  return idf;
+}
+
+function createMatrix (text) {
+  //Tokenize Sentences
+  var sentences = sentenceTokinization(text);
+  var numberOfWords = [];
+  for (let i = 0; i < sentences.length; i++) {
+    let words = generate_ngrams(tokenizeWords(sentences[i]), nGramNumber);
+    numberOfWords.push(words.length);
+  }
+  var numberOfColumns = Math.max(...numberOfWords);
+  var matrix = [];
+  for (let i = 0; i < sentences.length; i++) {
+    let words = generate_ngrams(tokenizeWords(sentences[i]), nGramNumber);
+    let thisSentence = [];
+    for (let k = 0; k < numberOfColumns; k++) {
+      if (words[k]) {
+        let tf = tfScore(words[k], sentences[i]);
+        let idf = idfScore(words[k], sentences);
+        let tfidfValue = tf * idf;
+        thisSentence.push(tfidfValue);
+      } else {
+        thisSentence.push(0);
+      }
+    }
+    matrix.push(thisSentence);
+  }
+  return matrix;
+}
+
+function zeros (columns, rows) {
+  var matrix = [];
+  var rowMatrix = [];
+  for (let i = 0; i < columns; i++) {
+    for (let k = 0; k < rows; k++) {
+      rowMatrix = [];
+      rowMatrix.push(0);
+    }
+    matrix.push(rowMatrix);
+  }
+  return matrix;
+}
+
+function cosineSimilarity (matrix) {
+  var resultMatrix = zeros(matrix.length, matrix[0].length);
+  var rowCount = 0;
+  var columnCount = 0;
+  for (let i = 0; i < matrix.length; i++) {
+    var A = matrix[i];
+    columnCount = 0;
+    for (let k = 0; k < matrix.length; k++) {
+      var B = matrix[k];
+      abDotProduct = dot(A, B);
+      denominator = Math.sqrt(dot(A, A)) * Math.sqrt(dot(B, B));
+      cosTheta = abDotProduct / denominator;
+      resultMatrix[rowCount][columnCount] = cosTheta;
+      columnCount++;
+    }
+    rowCount++;
+  }
+  var sentenceImportance = {};
+  var count = 0;
+  for (let i = 0; i < resultMatrix.length; i++) {
+    sentenceImportance[count] = resultMatrix[i].reduce((a, b) => a + b, 0) / resultMatrix[i].length;
+    count++;
+  }
+  return sentenceImportance;
+}
+
+function sortObjectEntries(obj, n){
+  let sortedList = []
+  //Sorting by values asc
+  sortedList = Object.entries(obj).sort((a,b)=>{
+      if(b[1] > a[1]) return 1;
+      else if(b[1] < a[1]) return -1;
+  //if values are same do edition checking if keys are in the right order
+      else {
+         if(a[0] > b[0]) return 1;
+         else if(a[0] < b[0]) return -1;
+         else return 0
+  }
+  })
+  // return first n values from sortedList
+  return sortedList.map(el=>el[0]).slice(0,n)
+}
+
+function dumas (text, k) {
+  var matrix = createMatrix(text);
+  var cosSim = cosineSimilarity(matrix);
+  var importantSentencesArray = sortObjectEntries(cosSim, k);
+  var sentences = sentenceTokinization(text);
+  var importantSentences = [];
+  for (let i = 0; i < k; i++) {
+    importantSentences.push(sentences[importantSentencesArray[i]]);
+  }
+  return importantSentences.join(' ');
+}
+
+console.log(dumas(wikiText, 6));
